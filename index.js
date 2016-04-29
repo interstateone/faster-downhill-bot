cede4b70468abd678f53d57dc7935d72b5aad1b7
Brandon Evans <brandon@brandonevans.ca>
2016-04-28 19:29:19 -0600

    Don't show text in attachment

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ controller.hears(['where'], ['direct_message', 'direct_mention'], function (bot,
 		var text = 'I\'m' + prefix + location
 		var imageURL = 'https://maps.googleapis.com/maps/api/staticmap?center=' + encodeURIComponent(location) + '&zoom=6&size=600x300&maptype=roadmap&key=' + mapsToken
 		var attachments = [{
-			text: text,
 			fallback: text,
 			pretext: ':world_map: ' + text,
 			image_url: imageURL
